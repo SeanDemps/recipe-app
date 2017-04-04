7210f5e64ff8cc86134e8f16cdd66dc5c3cbd2b4
DempsSean <demps.sean@gmail.com>
2017-04-04 23:11:24 +0100

    removing old localhost host name, small styling fix

--- src/app/services/recipes.service.ts
+++ src/app/services/recipes.service.ts
@@ -8,11 +8,11 @@ export class RecipesService {
   constructor(private httpWrapperService: HttpWrapperService) { }
 
   public getRecipes(page = 0, limit = 10): Promise<any> {
-    return this.httpWrapperService.get(`http://localhost.popcorn.bskyb.com:3000/recipes?page=${page}&limit=${limit}`);
+    return this.httpWrapperService.get(`http://localhost:3000/recipes?page=${page}&limit=${limit}`);
   }
 
   public getRecipeDetails(recipeId: number) {
-      return this.httpWrapperService.get(`http://localhost.popcorn.bskyb.com:3000/recipe-detail?recipeId=${recipeId}&userId=1`);
+      return this.httpWrapperService.get(`http://localhost:3000/recipe-detail?recipeId=${recipeId}&userId=1`);
   }
 
 }
--- src/app/services/starred.service.ts
+++ src/app/services/starred.service.ts
@@ -10,15 +10,15 @@ export class StarredService {
   constructor(private httpWrapperService: HttpWrapperService) {}
 
   public getStarsFromServer(page = 0, limit = 10): Promise<Array<number>> {
-    return this.httpWrapperService.get(`http://localhost.popcorn.bskyb.com:3000/stars?page=${page}&limit=${limit}&userId=1`);
+    return this.httpWrapperService.get(`http://localhost:3000/stars?page=${page}&limit=${limit}&userId=1`);
   }
 
   public addToStars(id: number): Promise<any> {
-    return this.httpWrapperService.post(`http://localhost.popcorn.bskyb.com:3000/star/${id}?userId=1`);
+    return this.httpWrapperService.post(`http://localhost:3000/star/${id}?userId=1`);
   }
 
   public removeFromStars(id: number): Promise<any> {
-    return this.httpWrapperService.delete(`http://localhost.popcorn.bskyb.com:3000/star/${id}?userId=1`);
+    return this.httpWrapperService.delete(`http://localhost:3000/star/${id}?userId=1`);
   }
 
 }
